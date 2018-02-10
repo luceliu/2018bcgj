990bb354406545cb165e65a3fa44ede707517883
Barry Tran <tran.barry.bat060@gmail.com>
2018-02-10 08:03:27 -0800

    Create basic player death, enemy movement, etc. Made prefabs of all.

---
+++ Assets/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int EnemySpeed;
+    public int XMoveDirection;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
+        
+        if (hit.distance < 1f)
+	    {
+	        Flip();
+	    }
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            Debug.Log("I hit the player");
+            var player = col.gameObject.GetComponent<Player_Health>();
+            player.PlayerDies();
+        }
+    }
+
+    void Flip()
+    {
+        if (XMoveDirection > 0)
+        {
+            XMoveDirection = -1;
+        }
+        else
+        {
+            XMoveDirection = 1;
+        }
+    }
+}
---
+++ Assets/Player_Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Health : MonoBehaviour
+{
+
+    public bool hasDied;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    hasDied = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (transform.position.y < -8)
+	    {
+	        PlayerDies();
+	    }
+	}
+
+    public void PlayerDies()
+    {
+        SceneManager.LoadScene("DreamScene");
+        
+        // Doesn't have to reset scene. Can do take damage -> invuln
+    }
+}
