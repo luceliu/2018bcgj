8ae315b0abb1b69a163e8aba809e80cb45a1f1d3
Barry Tran <tran.barry.bat060@gmail.com>
2018-02-10 00:30:20 -0800

    Add gamecontroller

--- Assets/Scenes/DreamScene/DreamGameController.cs
+++ Assets/Scenes/DreamScene/DreamGameController.cs
@@ -2,15 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DreamGameController : MonoBehaviour {
+public class DreamGameController : MonoBehaviour
+{
+    public static DreamGameController Instance = null;
+
+    private int _tiredLevel;
+    private int _maxTiredLevel;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    ResetTiredLevel();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void PlayerDreamDeath()
+    {
+        _tiredLevel++;
+        
+        // Call back to Real World game controller
+        if (_tiredLevel >= _maxTiredLevel)
+        {
+            // do something
+        }
+    }
+
+    public void PlayerDreamSuccess()
+    {
+        ResetTiredLevel();
+
+        // Call back to Real World game controller
+    }
+
+
+    private void ResetTiredLevel()
+    {
+        _tiredLevel = 0;
+    }
 }
