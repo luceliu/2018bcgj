dfea4c9f2aecb4461370810a9594d96813e204d5
Luce <lucy_liu@live.ca>
2018-02-11 14:24:06 -0800

    GameController no longer a singleton

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,10 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    private static GameController ActualInstance;
+    public GameObject finishFlag;
+    public Enemy targetEnemy;
 
     void Awake()
     {
         VideoModeManager.SetDreamworld();
+        finishFlag.gameObject.SetActive(false);
+        targetEnemy = GameObject.Find("Enemy").GetComponent<Enemy>();
     }
 
     // Use this for initialization
@@ -28,5 +33,16 @@ public class GameController : MonoBehaviour
         GameData.Instance.BattleResult = battleWon;
         GameData.Instance.TookMelatonin = false;
         SceneManager.LoadScene(GameData.OverworldSceneName);
+    }
+
+    public void DefeatedTargetEnemy() // something else will call this 
+    {
+        // Set finish flag to be visible
+        finishFlag.gameObject.SetActive(true);
+    }
+
+    public void ReachFlagAndEndDream() // insert some victorious transition back to overworld
+    {
+
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,57 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ProBuilder2.Common;
 
 public class PlayerController : MonoBehaviour
 {
-	public GameObject bulletPrefab;
-	public int playerSpeed = 10;
-	private bool facingRight = true;
-	private bool facingUp = false;
-	public int playerJumpPower = 1250;
-	private float moveX;
-	public bool isGrounded;
-	// Reference to the Animator
-	private Animator anim;
-	private bool running;
+    public GameObject bulletPrefab;
+    public int playerSpeed = 10;
+    private bool facingRight = true;
+    private bool facingUp = false;
+    public int playerJumpPower = 1250;
+    private float moveX;
+    public bool isGrounded;
+    // Reference to the Animator
+    private Animator anim;
+    private bool running;
     public float bulletSpeed = 10;
     public float bulletDelay = 1;
     private bool canShoot = true;
 
-	void Start()
-	{
-		anim = GetComponent<Animator> ();
-		running = false;
-	}
-	// Update is called once per frame
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        running = false;
+    }
+    // Update is called once per frame
 
-	void Update ()
-	{
-		if (Input.GetButtonDown("Fire1")) {
-			Shoot ();
-		}
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
 
-		PlayerMove();
-		if (Input.GetAxis ("Horizontal") != 0)
-		{
-			running = true;
-		}
-		else {
-			running = false;
-		}
-		if (running == true) 
-		{
-			anim.SetBool ("isRunning", true);
-		}
-		if (running == false) {
-			anim.SetBool("isRunning", false);
-		}
-	}
+        PlayerMove();
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            running = true;
+        }
+        else
+        {
+            running = false;
+        }
+        if (running == true)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        if (running == false)
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
 
-	void PlayerMove()
-	{
-		// controls
-		moveX = Input.GetAxis("Horizontal");
+    void PlayerMove()
+    {
+        // controls
+        moveX = Input.GetAxis("Horizontal");
         if (Input.GetAxis("Vertical") > 0f)
         {
             facingUp = true;
@@ -61,90 +65,98 @@ public class PlayerController : MonoBehaviour
             facingUp = false;
         }
 
-		if (Input.GetButtonDown("Jump") && isGrounded == true)
-		{
-			Jump();
-		}
-		// animation
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            Jump();
+        }
+        // animation
 
-		// player direction
-		if (moveX < 0.0f && facingRight == true)
-		{
-			FlipPlayer();
-		}
-		else if (moveX > 0.0f && facingRight == false)
-		{
-			FlipPlayer();
-		}
+        // player direction
+        if (moveX < 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
 
-		// physics
-		gameObject.GetComponent<Rigidbody2D>().velocity =
-			new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
-	}
+        // physics
+        gameObject.GetComponent<Rigidbody2D>().velocity =
+            new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
 
-	void Jump()
-	{
-		// jumping code
-		GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
-		anim.SetTrigger ("StartJump");
-		isGrounded = false;
-	}
+    void Jump()
+    {
+        // jumping code
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+        anim.SetTrigger("StartJump");
+        isGrounded = false;
+    }
 
-	void FlipPlayer()
-	{
-		// Set boolean so we remember if we're facing right or not
-		facingRight = !facingRight;
-		Vector2 localScale = gameObject.transform.localScale;
-		localScale.x *= -1;
-		transform.localScale = localScale;
-	}
+    void FlipPlayer()
+    {
+        // Set boolean so we remember if we're facing right or not
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
 
-	void OnCollisionEnter2D(Collision2D col)
-	{
-		if (col.gameObject.tag == "ground")
-		{
-			isGrounded = true;
-		}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
     }
 
 
-	void Shoot()
-	{
-	    if (canShoot == true)
-	    {
-	        Vector3 bulletInitialPosition;
-	        Vector2 bulletVelocity;
+    void Shoot()
+    {
+        if (canShoot == true)
+        {
+            Vector3 bulletInitialPosition;
+            Vector2 bulletVelocity;
 
-	        canShoot = false;
+            canShoot = false;
 
-	        if (facingRight == true && facingUp == false)
-	        {
-	            bulletInitialPosition = new Vector3(transform.position.x + 1f, transform.position.y);
+            if (facingRight == true && facingUp == false)
+            {
+                bulletInitialPosition = new Vector3(transform.position.x + 1f, transform.position.y);
 
-	            bulletVelocity = transform.right * bulletSpeed;
-	        }
-	        else if (facingRight == false && facingUp == false)
-	        {
+                bulletVelocity = transform.right * bulletSpeed;
+            }
+            else if (facingRight == false && facingUp == false)
+            {
 
-	            bulletInitialPosition = new Vector3(transform.position.x - 1f, transform.position.y);
-	            bulletVelocity = -transform.right * bulletSpeed;
-	        }
-	        else
-	        {
-	            bulletInitialPosition = new Vector3(transform.position.x - 0.0f, transform.position.y + 1f);
-	            bulletVelocity = transform.up * bulletSpeed;
-	        }
+                bulletInitialPosition = new Vector3(transform.position.x - 1f, transform.position.y);
+                bulletVelocity = -transform.right * bulletSpeed;
+            }
+            else
+            {
+                bulletInitialPosition = new Vector3(transform.position.x - 0.0f, transform.position.y + 1f);
+                bulletVelocity = transform.up * bulletSpeed;
+            }
 
-	        var bullet = GameObject.Instantiate(bulletPrefab, bulletInitialPosition, Quaternion.identity);
-	        bullet.GetComponent<Rigidbody2D>().velocity = bulletVelocity;
+            var bullet = GameObject.Instantiate(bulletPrefab, bulletInitialPosition, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = bulletVelocity;
 
             // Don't let player shoot gain for timeDelay()
             Invoke("ResetBulletDelay", bulletDelay);
         }
-	}
+    }
 
     private void ResetBulletDelay()
     {
         canShoot = true;
-	}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == GameObject.Find("FinishFlag").GetComponent<Collider2D>())
+        {
+            GameController.Instance.ReachFlagAndEndDream();
+        }
+    }
 }
