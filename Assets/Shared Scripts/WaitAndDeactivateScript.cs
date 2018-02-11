af98d4e7ce05ca46e6f619a01cde912c99820879
XCVG <xcvgvcx@gmail.com>
2018-02-11 09:44:16 -0800

    close enough

---
+++ Assets/Shared Scripts/WaitAndDeactivateScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitAndDeactivateScript : MonoBehaviour
+{
+    public float TimeToStay;
+    public GameObject ObjectToDeactivate;
+
+    private float elapsed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+
+        if (elapsed >= TimeToStay)
+        {
+            if (ObjectToDeactivate == null)
+                this.gameObject.SetActive(false);
+            else
+            {
+                ObjectToDeactivate.SetActive(false);
+                this.enabled = false;
+            }
+
+        }
+
+    }
+}
